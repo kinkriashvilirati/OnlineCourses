76f3f01d5a988b8bd9d71136319d1a9895e0d6a2
Rati Kinkriashvili <kinkriashvilirati@gmail.com>
2026-04-03 23:54:00 +0400

    feat: add unauthenticated navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { GuestNavigation } from "./components/layout/GuestNavigation";
+
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-grayscale-100">
+      <GuestNavigation />
+      <main className="px-[177px] pt-[97px]">
+        <div className="h-[calc(100vh-97px)]" />
+      </main>
+    </div>
+  );
 }
 
 export default App;
