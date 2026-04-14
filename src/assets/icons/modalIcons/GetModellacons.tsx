dcfdde702404639a8d756d98f21c000c20cc5e7d
Rati Kinkriashvili <kinkriashvilirati@gmail.com>
2026-04-14 20:24:59 +0400

    fix: fix styling for enrollment card

---
+++ src/assets/icons/modalIcons/GetModellacons.tsx
@@ -0,0 +1,14 @@
+import complete from "./complete.svg";
+import success from "./success.svg";
+import user from "./user.svg";
+import warning from "./warning.svg";
+
+type IconsObjInteface = Record<string, string>;
+const GetModdelacons: IconsObjInteface = {
+  complete,
+  success,
+  user,
+  warning,
+};
+
+export default GetModdelacons;
