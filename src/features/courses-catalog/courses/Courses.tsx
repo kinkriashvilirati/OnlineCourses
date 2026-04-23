588c03803754e9636dfbc05ed9a445d60b8508c9
Rati Kinkriashvili <kinkriashvilirati@gmail.com>
2026-04-23 10:18:04 +0400

    feat: add responsive for the courses in course catalog

--- src/features/courses-catalog/courses/Courses.tsx
+++ src/features/courses-catalog/courses/Courses.tsx
@@ -7,7 +7,7 @@ type CoursesProps = {
 
 export default function Courses({ courses }: CoursesProps) {
   return (
-    <ul className="m-0  w-full grid list-none grid-cols-3 gap-6 p-0">
+    <ul className="m-0 w-full grid list-none grid-cols-3 max-tabletL:grid-cols-2 max-laptop:grid-cols-2 max-laptopS:grid-cols-3 max-mobile:grid-cols-1 gap-6 p-0">
       {courses.map((course) => (
         <li key={course.id}>
           <Course course={course} />
